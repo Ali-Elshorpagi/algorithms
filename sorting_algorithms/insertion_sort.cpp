bf69386c1693fce33b13872b5b39a89c90060c92
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-03-04 11:33:02 +0200

    insertion sort

---
+++ sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pii> vpii;
+typedef vector<char> vc;
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define Mesh_Ali (ios_base::sync_with_stdio(false), cin.tie(NULL))
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define cl(v) ((v).clear())
+#define edl '\n'
+#define fr(i, x, n) for (int i(x); i < n; ++i)
+#define fl(i, x, n) for (int i(x); i >= n; --i)
+#define fc(it, v) for (auto &(it) : (v))
+#define sq(x) (x) * (x)
+#define yes printf("YES\n")
+#define no printf("NO\n")
+
+/*
+ * # Complexities
+ *
+ * Best-Case Time      O(N)
+ * Worst-Case Time     O(N²)
+ * Worst-Case Space    O(1)
+ */
+
+void Insertion_Sort(vi &arr)
+{
+    int key, j, len(sz(arr));
+    for (int i(1); i < len; ++i)
+    {
+        key = arr[i], j = i - 1;
+        while (j > -1 && arr[j] > key) // shift right
+            arr[j + 1] = arr[j], --j;
+        arr[j + 1] = key;
+    }
+}
+
+void Insertion_Sort_Recursive(vi &arr, int size)
+{
+    if (size <= 1)
+        return;
+    Insertion_Sort_Recursive(arr, size - 1);
+    int last(arr[size - 1]), j(size - 2);
+    while (j > -1 && arr[j] > last)
+        arr[j + 1] = arr[j], --j;
+    arr[j + 1] = last;
+}
+
+void Solve()
+{
+    vi arr{11, 4, 11, 11, 9, 10, 2, 3, 1};
+    Insertion_Sort(arr);
+    fc(it, arr) cout << it << ' ';
+    // test functions here;
+    cout << edl << "DONE" << edl;
+}
+
+int main()
+{
+    Mesh_Ali;
+    // freopen("../test/input.txt", "r", stdin);
+    freopen("../test/output.txt", "w", stdout);
+    int tc(1);
+    // scanf("%d", &tc);
+    while (tc--)
+        Solve();
+    return (0);
+}
