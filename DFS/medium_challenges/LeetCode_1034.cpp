1eeb8b890e518310c431e476680caaa4d6c6a015
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-04-25 09:51:43 +0200

    :LeetCode_1034

---
+++ DFS/medium_challenges/LeetCode_1034.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define Mesh_Ali (ios_base::sync_with_stdio(false), cin.tie(NULL))
+#define sz(v) ((int)((v).size()))
+#define edl '\n'
+#define fr(i, x, n) for (int i(x); i < n; ++i)
+#define fc(it, v) for (auto &(it) : (v))
+
+// link : https://leetcode.com/problems/coloring-a-border/
+// code : leetcode 1034
+
+class Solution // O(N * M) time, O(N * M) space
+{
+    vi dr{-1, 0, 1, 0}; // Delta for: up, right, down, left
+    vi dc{0, 1, 0, -1};
+    int colur;
+
+public:
+    Solution() { Mesh_Ali; }
+    bool is_valid(int new_row, int new_col, vvi &grid)
+    {
+        if (new_row < 0 || new_row >= sz(grid) || new_col < 0 || new_col >= sz(grid[0]))
+            return false;
+        return true;
+    }
+    void dfs(vvi &grid, vvi &visited, int r, int c, int val)
+    {
+        if (!is_valid(r, c, grid) || grid[r][c] != val || visited[r][c])
+            return;
+        visited[r][c] = 1;
+        if (!r || !c || r >= sz(grid) - 1 || c >= sz(grid[0]) - 1)
+            grid[r][c] = colur;
+        if ((r + 1 <= sz(grid) - 1 && grid[r + 1][c] != val && !visited[r + 1][c]) ||
+            (r - 1 >= 0 && grid[r - 1][c] != val && !visited[r - 1][c]) ||
+            (c + 1 <= sz(grid[0]) - 1 && grid[r][c + 1] != val && !visited[r][c + 1]) ||
+            (c - 1 >= 0 && grid[r][c - 1] != val && !visited[r][c - 1]))
+            grid[r][c] = colur;
+        fr(d, 0, 4) dfs(grid, visited, r + dr[d], c + dc[d], val);
+    }
+    vvi colorBorder(vvi &grid, int row, int col, int color)
+    {
+        colur = color;
+        vvi visited(sz(grid), vi(sz(grid[0])));
+        dfs(grid, visited, row, col, grid[row][col]);
+        return grid;
+    }
+    void TEST()
+    {
+        vvi grid{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+        int row(1), col(1), color(2);
+        vvi ans = colorBorder(grid, row, col, color);
+        fc(i, ans)
+        {
+            fc(j, i)
+            {
+                cout << j << ' ';
+            }
+            cout << edl;
+        }
+        // Output: [[2,2,2],[2,1,2],[2,2,2]]
+    }
+};
+
+int main()
+{
+    Solution sol;
+    // freopen("../../test/input.txt", "r", stdin);
+    freopen("../../test/output.txt", "w", stdout);
+    int tc(1);
+    // cin >> tc;
+    while (tc--)
+    {
+        cout << "Case #" << tc + 1 << edl;
+        sol.TEST();
+        cout << edl << "DONE" << edl;
+    }
+    return (0);
+}
