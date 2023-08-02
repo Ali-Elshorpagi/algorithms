33dc1d5a1b87675a92329427ba41f7de6f067175
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-08-02 04:36:01 +0300

    House Robber - LeetCode 198

---
+++ DP/pick_or_leave_pattern/LeetCode_198.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define Mesh_Ali (ios_base::sync_with_stdio(false), cin.tie(NULL))
+#define sz(v) ((int)((v).size()))
+#define edl '\n'
+
+// link : https://leetcode.com/problems/house-robber/
+// name :  House Robber - LeetCode 198
+
+class Solution
+{
+    vi memory; // for memoization
+public:
+    Solution() { Mesh_Ali, memory.resize(405, -1); }
+    int dp(vi &nums, int idx)
+    {
+        if (idx >= sz(nums))
+            return 0;
+        auto &ref(memory[idx]);
+        if (ref != -1)
+            return ref;
+        int pick(nums[idx] + dp(nums, idx + 2)), leave(dp(nums, idx + 1));
+        return ref = max(pick, leave);
+    }
+    int rob(vi &nums) { return dp(nums, 0); }
+    void TEST()
+    {
+        // vi nums{2, 7, 9, 3, 1};
+        // cout << rob(nums) << edl; // 12
+        vi nums{1, 2, 3, 1};
+        cout << rob(nums) << edl; // 4
+    }
+};
+
+int main()
+{
+    Solution sol;
+    // freopen("../../test/input.txt", "r", stdin);
+    freopen("../../test/output.txt", "w", stdout);
+    int tc(1);
+    // cin >> tc;
+    while (tc--)
+        cout << "Case #" << tc + 1 << edl, sol.TEST();
+    cout << edl << "DONE" << edl;
+    return (0);
+}
