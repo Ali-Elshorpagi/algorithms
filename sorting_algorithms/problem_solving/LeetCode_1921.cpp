e8ffd6a417f5ec3a3e00f83d04bef21211546425
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-08-05 09:10:17 +0300

    update sorting

--- sorting_algorithms/problem_solving/LeetCode_1921.cpp
+++ sorting_algorithms/problem_solving/LeetCode_1921.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
 typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<ll> vll;
-typedef vector<vll> vvll;
-typedef vector<pii> vpii;
-typedef vector<char> vc;
 
 #define _CRT_SECURE_NO_DEPRECATE
 #define Mesh_Ali (ios_base::sync_with_stdio(false), cin.tie(NULL))
 #define all(v) ((v).begin()), ((v).end())
 #define sz(v) ((int)((v).size()))
-#define cl(v) ((v).clear())
 #define edl '\n'
 #define fr(i, x, n) for (int i(x); i < n; ++i)
-#define fl(i, x, n) for (int i(x); i > n; --i)
-#define fc(it, v) for (auto &(it) : (v))
-#define sq(x) (x) * (x)
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 
 class Solution
 {
 public:
     Solution() { Mesh_Ali; }
-    int eliminateMaximum(vi &dist, vi &speed) {}
+    int eliminateMaximum(vi &dist, vi &speed)
+    {
+        int len(sz(dist));
+        vector<double> time(len);
+        fr(i, 0, len) { time[i] = ceil((double)dist[i] / speed[i]); }
+        sort(all(time));
+        int cnt(0);
+        fr(i, 0, len)
+        {
+            if (i >= time[i]) // i here equal my-time
+                break;
+            ++cnt;
+        }
+        return cnt;
+    }
     void TEST()
     {
         vi dist{1, 3, 4}, speed{1, 1, 1};
