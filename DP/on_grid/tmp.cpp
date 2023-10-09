eebb845d4896e80f6d8e945ffe473738abf98959
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-10-09 04:41:33 +0300

    another sol to leetcode 174

--- DP/on_grid/tmp.cpp
+++ DP/on_grid/tmp.cpp
@@ -22,23 +22,6 @@ public:
             return false;
         return true;
     }
-    int dp(vvi &matrix, int row, int col)
-    {
-        if (row == sz(matrix) - 1 && col == sz(matrix[0]) - 1)
-            return matrix[row][col] < 0 ? -matrix[row][col] + 1 : 1; // max(1, -matrix[row][col] + 1);
-
-        if (!is_valid(matrix, row, col))
-            return 1e9;
-
-        auto &ref(memory[row][col]);
-        if (ref != -1)
-            return ref;
-
-        int go_right(max(1, dp(matrix, row, col + 1) - matrix[row][col])),
-            go_down(max(1, dp(matrix, row + 1, col) - matrix[row][col]));
-
-        return ref = min(go_right, go_down);
-    }
     bool dp_evaluate(vvi &dungeon, int row, int col, int mid)
     {
         if (!is_valid(dungeon, row, col) || mid <= 0)
@@ -70,11 +53,7 @@ public:
         }
         return ans;
     }
-    int calculateMinimumHP(vvi &dungeon)
-    {
-        // return dp(dungeon, 0, 0);
-        return binary_search(dungeon);
-    }
+    int calculateMinimumHP(vvi &dungeon) { return binary_search(dungeon); }
     void TEST()
     {
         vvi dungeon{
