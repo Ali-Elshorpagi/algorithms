aa28bdf18a9e716e47336261ed0a5471e79349cd
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-11-03 15:59:38 +0200

    solveSudoku

--- backtracking/LeetCode_37.cpp
+++ backtracking/LeetCode_37.cpp
@@ -10,6 +10,7 @@ typedef vector<vc> vvc;
 #define Sukuna (ios_base::sync_with_stdio(false), cin.tie(NULL))
 #define sz(v) ((int)((v).size()))
 #define edl '\n'
+#define fr(i, x, n) for (int i(x); i < n; ++i)
 #define fc(it, v) for (auto &(it) : (v))
 #define yes cout << "YES\n"
 #define no cout << "NO\n"
@@ -18,8 +19,44 @@ class Solution
 {
 public:
     Solution() { Sukuna; }
-    bool is_valid(vvc &board, int row, int col, char ch) { return true; }
-    void solveSudoku(vvc &board) {}
+    bool is_valid(vvc &board, int row, int col, char ch)
+    {
+        fr(i, 0, 9)
+        {
+            // It ensures that the same character is not present in the same row, column,
+            if (board[i][col] == ch || board[row][i] == ch)
+                return false;
+            // It ensures that the same character is not present in the 3x3 subgrid of the specified position.
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch)
+                return false;
+        }
+        return true;
+    }
+    bool backtracking(vvc &board)
+    {
+        fr(i, 0, sz(board))
+        {
+            fr(j, 0, sz(board[0]))
+            {
+                if (board[i][j] == '.')
+                {
+                    for (char ch('1'); ch <= '9'; ++ch)
+                    {
+                        if (is_valid(board, i, j, ch))
+                        {
+                            board[i][j] = ch;
+                            if (backtracking(board))
+                                return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vvc &board) { backtracking(board); }
     void TEST()
     {
         vvc board{{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
@@ -41,7 +78,12 @@ public:
                    {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
                    {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
                    {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
-        result == board ? yes : no;
+        result == board ? yes : no; // YES
+        fc(it, board)
+        {
+            fc(jt, it) { cout << jt << ' '; }
+            cout << edl;
+        }
     }
 };
 
