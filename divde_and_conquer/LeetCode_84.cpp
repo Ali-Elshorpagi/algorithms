77911fbe18f9a4d2508aed3a535a14ea5b97fe15
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2024-01-03 00:43:53 +0200

    leetCode 84 v1

--- divde_and_conquer/LeetCode_84.cpp
+++ divde_and_conquer/LeetCode_84.cpp
@@ -1,31 +1,70 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 typedef vector<int> vi;
-typedef long long ll;
 
 #define _CRT_SECURE_NO_DEPRECATE
 #define Sukuna (ios_base::sync_with_stdio(false), cin.tie(NULL))
 #define sz(v) ((int)((v).size()))
 #define edl '\n'
-#define fr(i, x, n) for (int i(x); i < n; ++i)
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 
 class Solution
 {
 
 public:
     Solution() { Sukuna; }
-    int largestRectangleArea(vi &heights) {}
+    int max_mid_area(vi &heights, int start, int end, int mid)
+    {
+        int i(mid), j(mid + 1);
+        int max_area(0), height(min(heights[i], heights[j]));
+        while (i >= start && j <= end)
+        {
+            height = min({height, heights[i], heights[j]});
+            int width(j - i + 1),
+                current_area(width * height);
+            max_area = max(max_area, current_area);
+            if (i == start)
+                ++j;
+            else if (j == end)
+                --i;
+            else
+            {
+                if (heights[i - 1] > heights[j + 1])
+                    --i;
+                else
+                    ++j;
+            }
+        }
+        return max_area;
+    }
+    int max_area(vi &heights, int start, int end)
+    {
+        if (start == end)
+            return heights[start];
+        int mid(start + ((end - start) >> 1));
+        // max area from left half
+        int area(max_area(heights, start, mid));
+        // max area from right half
+        area = max_area(heights, mid + 1, end);
+        // max area across the middle
+        area = max(area, max_mid_area(heights, start, end, mid));
+        return area;
+    }
+    int largestRectangleArea(vi &heights)
+    {
+        if (!sz(heights))
+            return 0;
+        return max_area(heights, 0, sz(heights) - 1);
+    }
     void TEST()
     {
         vi heights{2, 1, 5, 6, 2, 3};
-        cout << largestRectangleArea(nums) << edl; // 10
+        cout << largestRectangleArea(heights) << edl; // 10
         heights = {2, 4};
-        cout << largestRectangleArea(nums) << edl; // 4
+        cout << largestRectangleArea(heights) << edl; // 4
     }
 };
 
--- divde_and_conquer/README.md
+++ divde_and_conquer/README.md
@@ -3,3 +3,4 @@
 |--|----|-------- | ---------- | -------| ------ | ---- |
 |00|775|[Global and Local Inversions](https://leetcode.com/problems/global-and-local-inversions/)|[C++](https://github.com/Ali-Elshorpagi/algorithms/blob/main/divde_and_conquer/LeetCode_775.cpp)|`O(N * log(N))`|`O(N)`|Merge Sort|
 |01|50|[Pow(x, n)](https://leetcode.com/problems/powx-n/)|[C++](https://github.com/Ali-Elshorpagi/algorithms/blob/main/divde_and_conquer/LeetCode_50.cpp)|`O(log(N))`|`O(N)`|Divde & Conquer|
+|02|84|[Largest Rectangle in Histogram](https://leetcode.com/problems/largest-rectangle-in-histogram/)|[C++](https://github.com/Ali-Elshorpagi/algorithms/blob/main/divde_and_conquer/LeetCode_84.cpp)|`O(N * log(N))`|`O(log(N))`|Divde & Conquer|
